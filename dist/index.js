47dc002ff5b595c523f6b88a393f8ff4c371fd95
remal <remal@users.noreply.github.com>
2023-10-10 20:16:13 +0000

    [push-back] Push-back updated files during build

--- dist/index.js
+++ dist/index.js
@@ -57,7 +57,7 @@ function newOctokitInstance(token) {
                 return retryCount <= 4;
             },
             onSecondaryRateLimit: (retryAfter, options) => {
-                core.warning(`Abuse detected for request ${options.method} ${options.url}`);
+                core.error(`Abuse detected for request ${options.method} ${options.url}`);
                 return false;
             },
         },
@@ -79,10 +79,11 @@ function newOctokitInstance(token) {
         ...logOptions,
     };
     const octokit = new OctokitWithPlugins(allOptions);
-    return {
+    const client = {
         ...octokit.rest,
         paginate: octokit.paginate,
     };
+    return client;
 }
 exports.newOctokitInstance = newOctokitInstance;
 
